f44c5a27018ee392899982e154cde4018c688b6c
SgtPlatinum <isluckin@gmail.com>
2023-04-18 15:55:20 +0300

    quick fix of pageinfo desc

--- src/lib/component/PageInfo.svelte
+++ src/lib/component/PageInfo.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<!-- TODO: make a block of short info about the website
+<!-- TODO: make a block of short info about the website and put in product page right below the header
      example: https://lechattravy.ru/ -->
 
 <style lang="postcss">
